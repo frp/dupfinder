e733c5cc599834de4e17ea485ac573412a543937
Roman Franchuk <franchukrom@gmail.com>
2015-05-31 01:25:37 +0300

    Basic implementation of db datastore
    Currently using H2 memory database and suitable for testing purposes
    only.
    
    The implementation is the most basic and unoptimized. In the future,
    think of a better implementation.

--- src/main/scala/DbDataStore.scala
+++ src/main/scala/DbDataStore.scala
@@ -8,7 +8,6 @@ import scala.collection.mutable.{Set, Map}
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import slick.driver.H2Driver.api._
-import scala.concurrent.ExecutionContext.Implicits.global
 
 class FileHashes(tag: Tag) extends Table[(Option[Int], String, String)](tag, "file_hashes") {
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
@@ -27,32 +26,47 @@ class HashToCounts(tag: Tag) extends Table[(Option[Int], String, Int)](tag, "has
 }
 
 // TODO: think of more asynchronous way of dealing with things
+// TODO: optimize this, a lot
 // May need changes to DataStore interface
 
 class DbDataStore extends DataStore {
   private val db = Database.forConfig("h2mem1")
   private val hashes = TableQuery[FileHashes]
-  private val hashToCount = TableQuery[HashToCounts]
 
   {
-    val setup = hashes.schema.create
+    val setup = DBIO.seq(
+      //hashes.schema.drop,
+      hashes.schema.create
+    )
     Await.ready(db.run(setup), Duration.Inf)
   }
 
   override def addFile(file: String, hash: String): Unit = {
-    Await.ready(db.run(hashes += (None, file, hash)), Duration.Inf)
-
+    removeFile(file)
+    Await.ready(db.run(hashes.insertOrUpdate(None, file, hash)), Duration.Inf)
   }
 
   override def getAllDupesets: Map[String, Set[String]] = {
-    Map[String, Set[String]]()
+    val q = for {
+      hash1 <- hashes.groupBy(_.hash).map({case (hash, hashes) => (hash, hashes.length)}).filter(_._2 > 1).map(_._1) // list of hashes with dupes
+      (file_name, hash) <- hashes.map(x => (x.name, x.hash)) // map: file name -> hash
+      if hash1 === hash
+    }
+      yield (hash, file_name)
+
+    val result = Map[String, Set[String]]()
+    for ((hash, name) <- Await.result(db.run(q.result), Duration.Inf))
+      result.getOrElseUpdate(hash, Set[String]()) += name
+    result
   }
 
   override def countDupes(file: String): Int = {
-    0
+    val q = hashes.filter(_.name === file).map(_.hash)
+    val hash = Await.result(db.run(q.result), Duration.Inf)(0)
+    Await.result(db.run(hashes.filter(_.hash === hash).length.result), Duration.Inf)
   }
 
-  override def removeFile(file: String): Unit = {
-
+  override def removeFile(file: String) {
+    Await.ready(db.run(hashes.filter(_.name === file).delete), Duration.Inf)
   }
 }
--- src/test/scala/DataStoreTest.scala
+++ src/test/scala/DataStoreTest.scala
@@ -4,10 +4,12 @@ import collection.mutable.Map
 import collection.mutable.Set
 import org.scalatest._
 
-class DataStoreTest extends FlatSpec with Matchers {
+abstract class DataStoreTest extends FlatSpec with Matchers {
 
-  trait MemDataStore {
-    val store = new MemoryDataStore
+  def getStore: DataStore
+
+  trait DataStoreFixture {
+    val store = getStore
 
     val data = Array(
       ("fileWithNoDupes", sha1Hex("data1")),
@@ -22,41 +24,49 @@ class DataStoreTest extends FlatSpec with Matchers {
       store.addFile(file, hash)
   }
 
-  "countOfDupes()" should "return count of dupes for a file name" in new MemDataStore {
+  "countOfDupes()" should "return count of dupes for a file name" in new DataStoreFixture {
     store.countDupes("fileWithNoDupes") should be(1)
     store.countDupes("fileWithADupe1") should be(2)
     store.countDupes("fileWithADupe2") should be(2)
     store.countDupes("fileWith2Dupes1") should be(3)
   }
 
-  "getAllDupesets()" should "return a map: hash -> set of names of files" in new MemDataStore {
-    store.getAllDupesets should be (Map[String, Set[String]](
+  "getAllDupesets()" should "return a map: hash -> set of names of files" in new DataStoreFixture {
+    store.getAllDupesets should be(Map[String, Set[String]](
       sha1Hex("data2") -> Set("fileWithADupe1", "fileWithADupe2"),
       sha1Hex("data3") -> Set("fileWith2Dupes1", "fileWith2Dupes2", "fileWith2Dupes3")
     ))
   }
 
-  "removeFile()" should "remove file from dupe lists correctly" in new MemDataStore {
+  "removeFile()" should "remove file from dupe lists correctly" in new DataStoreFixture {
     store.removeFile("fileWithADupe1")
-    store.getAllDupesets should be (Map[String, Set[String]](
+    store.getAllDupesets should be(Map[String, Set[String]](
       sha1Hex("data3") -> Set("fileWith2Dupes1", "fileWith2Dupes2", "fileWith2Dupes3")
     ))
   }
 
-  "addFile()" should "process repeated files correctly" in new MemDataStore {
+  "addFile()" should "process repeated files correctly" in new DataStoreFixture {
     store.addFile("fileWithNoDupes", sha1Hex("data4"))
     store.countDupes("fileWithNoDupes") should be(1)
 
-    store.getAllDupesets should be (Map[String, Set[String]](
+    store.getAllDupesets should be(Map[String, Set[String]](
       sha1Hex("data2") -> Set("fileWithADupe1", "fileWithADupe2"),
       sha1Hex("data3") -> Set("fileWith2Dupes1", "fileWith2Dupes2", "fileWith2Dupes3")
     ))
   }
 
-  "addFile()" should "process the situation when the updated version of a file is not a dupe anymore" in new MemDataStore {
+  "addFile()" should "process the situation when the updated version of a file is not a dupe anymore" in new DataStoreFixture {
     store.addFile("fileWithADupe1", sha1Hex("data5"))
-    store.getAllDupesets should be (Map[String, Set[String]](
+    store.getAllDupesets should be(Map[String, Set[String]](
       sha1Hex("data3") -> Set("fileWith2Dupes1", "fileWith2Dupes2", "fileWith2Dupes3")
     ))
   }
+}
+
+class MemoryDataStoreTest extends DataStoreTest {
+  def getStore = new MemoryDataStore
+}
+
+class DbDataStoretest extends DataStoreTest {
+  def getStore = new DbDataStore
 }
